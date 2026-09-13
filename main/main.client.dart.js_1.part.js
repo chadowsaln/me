0fdb769d25e93b07846c1bd34436bac91cf230c3
chadowsaln <shadowdev674@gmail.com>
2026-09-13 21:20:43 +0000

    Serve Jaspr static build from main/ for Vercel
    - copy build/jaspr output into committed main/ (relative asset paths)
    - vercel.json: framework null, no build, outputDirectory main

---
+++ main/main.client.dart.js_1.part.js
@@ -0,0 +1,103 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,C,A={
+dL(d,e,f){return new A.dK(f,e,d,null)},
+dK:function dK(d,e,f,g){var _=this
+_.d=d
+_.f=e
+_.w=f
+_.a=g},
+dN:function dN(d,e){this.w=d
+this.a=e},
+dO:function dO(d,e,f){this.d=d
+this.w=e
+this.a=f},
+bx:function bx(d,e){this.b=d
+this.a=e},
+dv:function dv(d,e,f,g,h,i,j){var _=this
+_.d$=d
+_.e$=e
+_.f$=f
+_.cy=null
+_.db=g
+_.c=_.b=_.a=null
+_.d=h
+_.e=null
+_.f=i
+_.w=_.r=null
+_.x=j
+_.Q=_.z=_.y=null
+_.as=!1
+_.at=!0
+_.ax=!1
+_.CW=null
+_.cx=!1},
+a4:function a4(){},
+d8:function d8(d,e,f){var _=this
+_.c=_.b=_.a=_.cy=_.ry=null
+_.d=d
+_.e=null
+_.f=e
+_.w=_.r=null
+_.x=f
+_.Q=_.z=_.y=null
+_.as=!1
+_.at=!0
+_.ax=!1
+_.CW=null
+_.cx=!1},
+j1(){return new A.ay(null)},
+ay:function ay(d){this.a=d},
+d_:function d_(d){this.a=d}},D
+B=c[0]
+C=c[2]
+A=a.updateHolder(c[3],A)
+D=c[4]
+A.dK.prototype={
+a0(d){var x=null
+return new B.ab("div",x,this.d,x,this.f,x,this.w,x)}}
+A.dN.prototype={
+a0(d){var x=null
+return new B.ab("p",x,x,x,x,x,this.w,x)}}
+A.dO.prototype={
+a0(d){var x=null
+return new B.ab("span",x,this.d,x,x,x,this.w,x)}}
+A.bx.prototype={
+O(){var x=B.aY(y.b),w=($.H+1)%16777215
+$.H=w
+return new A.dv(null,!1,!1,x,w,this,C.c)}}
+A.dv.prototype={
+a1(){var x=this.f
+x.toString
+return y.B.a(x).b},
+P(){var x=this.CW.d$
+x.toString
+return B.jb(x,null)},
+Y(d){y.u.a(d)}}
+A.a4.prototype={
+O(){var x=($.H+1)%16777215
+$.H=x
+return new A.d8(x,this,C.c)}}
+A.d8.prototype={
+gk(){return y.i.a(B.d.prototype.gk.call(this))},
+D(){if(this.w.c)this.r.toString
+this.b2()},
+ac(d){y.i.a(B.d.prototype.gk.call(this))
+return!0},
+bq(){return y.i.a(B.d.prototype.gk.call(this)).a0(this)},
+a7(){this.w.toString
+this.bL()}}
+A.ay.prototype={
+a0(d){var x=null,w=y.F
+return A.dL(B.f([D.I,A.dL(B.f([A.dL(B.f([new A.dO("scaffold-note-dot",B.f([],w),x),new A.dN(B.f([new B.b4("Shadow Dev \u2014 portfolio sections land on the next pass.",x)],w),x)],w),x,"scaffold-note")],w),x,"container")],w),x,"site")}}
+A.d_.prototype={
+a0(d){var x=y.F,w=y.w
+return new A.bx(B.f([A.dL(B.f([],x),B.ei(["aria-hidden","true"],w,w),"page-bg"),A.dL(B.f([],x),B.ei(["aria-hidden","true"],w,w),"page-grid")],x),null)}}
+var z=a.updateTypes([]);(function inheritance(){var x=a.inheritMany,w=a.inherit
+x(B.r,[A.a4,A.bx])
+x(A.a4,[A.dK,A.dN,A.dO,A.ay,A.d_])
+w(A.dv,B.aK)
+w(A.d8,B.aW)})()
+B.i6(b.typeUniverse,JSON.parse('{"dK":{"a4":[],"r":[]},"dN":{"a4":[],"r":[]},"dO":{"a4":[],"r":[]},"bx":{"r":[]},"dv":{"S":[],"d":[],"J":[]},"a4":{"r":[]},"d8":{"d":[],"J":[]},"ay":{"a4":[],"r":[]},"d_":{"a4":[],"r":[]}}'))
+var y={b:B.a8("d"),B:B.a8("bx"),F:B.a8("w<r>"),u:B.a8("fS"),i:B.a8("a4"),w:B.a8("b")};(function constants(){D.I=new A.d_(null)})()};
+(a=>{a["za2GxXypjFtOjMFC+2RNgLHRrmE="]=a.current})($__dart_deferred_initializers__);
+//# sourceMappingURL=main.client.dart.js_1.part.js.map
